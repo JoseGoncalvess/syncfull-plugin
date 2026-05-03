431e5f1b00b9614f364d33c3b0aefdcfb709e469
JoseGoncalvess <joseinacio774@gmail.com>
2026-05-02 22:10:06 -0300

    fix: ajuste builder dev

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -37,7 +37,7 @@ const context = await esbuild.context({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outfile: "dist/main.js",
+	outfile: prod ? "dist/main.js" : "main.js",
 	minify: prod,
 });
 
